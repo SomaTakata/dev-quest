f168cac41f67e974f75db067c9c90e75f9406424
Soma Takata <hayatetaka123@gmail.com>
2024-01-26 15:28:56 +0900

    feat: 🎸 create first layout

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,8 +2,17 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="flex h-full">
+      <div className="h-full w-[280px] bg-[#5A57FF]">
+      </div>
+      <div className="w-full ">
+      <div className="bg-white w-full h-16 flex items-center justify-end p-5">
       <UserButton afterSignOutUrl="/sign-in" />
+      </div>
+      <div className="h-full bg-[#FAF9FF]">
+
+      </div>
+      </div>
     </div>
   )
 }
