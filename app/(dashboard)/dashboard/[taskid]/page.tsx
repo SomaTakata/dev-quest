eabd4aa24f5e5fecdab317b209302e3d16e68994
Soma Takata <hayatetaka123@gmail.com>
2024-01-28 01:37:55 +0900

    feat: 🎸 add click x to go to dashboard page

--- app/(dashboard)/dashboard/[taskid]/page.tsx
+++ app/(dashboard)/dashboard/[taskid]/page.tsx
@@ -1,18 +1,26 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { Plus, X } from "lucide-react";
 import React from "react";
 import QuestionCard from "../../_components/QuestionCard";
+import { useRouter } from "next/navigation";
 
 const taskHome = () => {
+  const router = useRouter();
   return (
     <div className="px-16">
-      <div className="flex justify-end mt-8">
+      <div className="flex justify-end mt-10">
         <div className="flex mr-20 gap-4 items-center">
           <p className="text-accent-foreground text-sm font-semibold">期限</p>
           <p className="text-[#8F8F8F] font-xs font-semibold">2023/02/16</p>
         </div>
-        <X size={36} />
+        <X
+          size={36}
+          onClick={() => {
+            router.push("/dashboard");
+          }}
+        />
       </div>
       <div className="flex flex-row justify-between items-center mb-6 mt-6">
         <h1 className="font-bold text-3xl underline ">株式会社MIXI</h1>
