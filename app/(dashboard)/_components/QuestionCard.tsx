2cf0f8aacd8977dffc11959c94a582da86712e34
Soma Takata <hayatetaka123@gmail.com>
2024-01-28 03:16:53 +0900

    feat: 🎸 add question area and button

--- app/(dashboard)/_components/QuestionCard.tsx
+++ app/(dashboard)/_components/QuestionCard.tsx
@@ -1,12 +1,21 @@
 "use client";
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { Check, Loader2, Square, Trash2 } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type ButtonStateType = "available" | "loading" | "completed";
+type CardStateType = "indeterminate" | "dug";
 type ButtonProperties = {
   [state in ButtonStateType]: {
     text: string;
@@ -16,10 +25,10 @@ type ButtonProperties = {
 };
 const buttonProperties: ButtonProperties = {
   available: {
-    text: "質問を深掘する",
+    text: "質問を深掘る",
   },
   loading: {
-    text: "深掘中",
+    text: "深掘り中",
     disabled: true,
     icon: <Loader2 className="mr-2 h-4 w-4 animate-spin" />,
   },
@@ -32,32 +41,123 @@ const buttonProperties: ButtonProperties = {
 
 const QuestionCard = () => {
   const [buttonState, setButtonState] = useState<ButtonStateType>("available");
-
+  const [cardState, setCardState] = useState<CardStateType>("indeterminate");
+  const [inputValue, setInputValue] = useState("");
+  const [isActive, setIsActive] = useState(false);
+  useEffect(() => {
+    setIsActive(inputValue.length > 0);
+  }, [inputValue]);
   return (
-    <Card className=" h-[178px] px-6 py-8 rounded-sm">
+    <Card className=" min-h-[178px] px-6 py-8 rounded-sm">
       <div className="flex">
-        <Checkbox className="border-secondary mt-1" />
-        <div className="grow mx-6 text-lg font-semibold leading-6">
-          <p>
-            問1)
-            MIXIのインターンシップで挑戦してみたいことや目的、目標を教えてください
-            500文字以内
-          </p>
-          <Button
-            className="mt-6 w-full text-base  bg-primary"
-            disabled={buttonProperties[buttonState].disabled}
-            onClick={() => {
-              setButtonState("loading");
-              setTimeout(() => {
-                setButtonState("completed");
-              }, 3000);
-            }}
-          >
-            {buttonProperties[buttonState].icon}
-            {buttonProperties[buttonState].text}
-          </Button>
+        <div className="w-full">
+          <div className="flex justify-between w-full gap-4 items-center">
+            <Checkbox className="border-secondary mt-1" />
+            <Textarea
+              className={`bg-inherit focus:border-none text-lg font-bold ${isActive ? "border-none " : ""}`}
+              placeholder="質問を入力してください。例 : 問1)MIXIのインターンシップで挑戦してみたいことや目的、目標を教えてください (500文字以内)"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onFocus={() => setIsActive(true)} // フォーカス時は常にborder-noneを適用
+              onBlur={() => setIsActive(inputValue.length > 0)} // フォーカスが外れた時に入力があるかどうかを確認
+            />
+            <Trash2 size={24} className="text-muted" />
+          </div>
+          <div className="px-8">
+            <div className={cardState === "indeterminate" ? "" : "hidden"}>
+              <Button
+                className="mt-6 w-full text-base  bg-primary"
+                disabled={buttonProperties[buttonState].disabled}
+                onClick={() => {
+                  setButtonState("loading");
+                  setTimeout(() => {
+                    setButtonState("completed");
+
+                    setTimeout(() => {
+                      setCardState("dug");
+                    }, 1000);
+                  }, 3000);
+                }}
+              >
+                {buttonProperties[buttonState].icon}
+                {buttonProperties[buttonState].text}
+              </Button>
+            </div>
+            <div className={cardState === "dug" ? "" : "hidden"}>
+              <p className="mt-6 text-lg mb-4 font-bold text-[#BEBEC1]">
+                以下の3つから回答したい質問を選択してください。
+              </p>
+              <Accordion type="multiple">
+                <AccordionItem
+                  value="item-1"
+                  className="bg-primary text-primary-foreground"
+                >
+                  <AccordionTrigger className="font-medium ">
+                    なぜあなたはインターンに参加したいのですか？
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="px-4 ">
+                      <Textarea
+                        className={`bg-[#FFFFFF] text-secondary py-2`}
+                        placeholder="回答を記入してください"
+                        //中身のvalueをstateで持つ
+                      />
+                      <div className="flex justify-end mt-4">
+                        <Button className="px-12 font-bold text-primary-foreground hover:text-primary hover:bg-primary-foreground border border-primary-foreground">
+                          深掘る
+                        </Button>
+                      </div>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem
+                  value="item-2"
+                  className="bg-primary text-primary-foreground"
+                >
+                  <AccordionTrigger className="font-medium">
+                    なぜあなたはインターンに参加したいのですか？
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="px-4">
+                      <Textarea
+                        className={`bg-[#FFFFFF] text-secondary py-2`}
+                        placeholder="回答を記入してください"
+                        //中身のvalueをstateで持つ
+                      />
+                      <div className="flex justify-end mt-4">
+                        <Button className="px-12 font-bold text-primary-foreground hover:text-primary hover:bg-primary-foreground border border-primary-foreground">
+                          深掘る
+                        </Button>
+                      </div>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem
+                  value="item-3"
+                  className="bg-primary text-primary-foreground"
+                >
+                  <AccordionTrigger className="font-medium">
+                    なぜあなたはインターンに参加したいのですか？
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="px-4 ">
+                      <Textarea
+                        className={`bg-[#FFFFFF] text-secondary py-2`}
+                        placeholder="回答を記入してください"
+                        //中身のvalueをstateで持つ
+                      />
+                      <div className="flex justify-end mt-4">
+                        <Button className="px-12 font-bold text-primary-foreground hover:text-primary hover:bg-primary-foreground border-2 border-primary-foreground">
+                          深掘る
+                        </Button>
+                      </div>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </div>
+          </div>
         </div>
-        <Trash2 size={24} className="text-muted" />
       </div>
     </Card>
   );
--- app/(dashboard)/dashboard/[taskid]/page.tsx
+++ app/(dashboard)/dashboard/[taskid]/page.tsx
@@ -9,12 +9,15 @@ import { useRouter } from "next/navigation";
 const taskHome = () => {
   const router = useRouter();
   return (
-    <div className="px-16">
-      <div className="flex justify-end mt-10">
-        <div className="flex mr-20 gap-4 items-center">
+    <div className="px-16 py-4">
+      <div className="flex justify-end ">
+        <div className="flex  gap-4 items-center">
           <p className="text-accent-foreground text-sm font-semibold">期限</p>
           <p className="text-[#8F8F8F] font-xs font-semibold">2023/02/16</p>
         </div>
+      </div>
+      <div className="flex flex-row justify-between items-center mb-8 mt-6">
+        <h1 className="font-bold text-3xl underline ">株式会社MIXI</h1>
         <X
           size={36}
           onClick={() => {
@@ -22,10 +25,7 @@ const taskHome = () => {
           }}
         />
       </div>
-      <div className="flex flex-row justify-between items-center mb-6 mt-6">
-        <h1 className="font-bold text-3xl underline ">株式会社MIXI</h1>
-      </div>
-      <div className="flex gap-8 items-center w-full">
+      <div className="px-2  flex gap-8 items-center w-full">
         <p className=" text-sm font-bold text-accent-foreground text-nowrap">
           進捗
         </p>
@@ -33,14 +33,16 @@ const taskHome = () => {
         <p className="font-bold text-[#BEBEC1]">33%</p>
       </div>
 
-      <div className="px-4 w-full flex justify-center">
+      <div className="px- w-full flex justify-center">
         <div className="mt-10 w-full">
           <p className=" text-xl font-bold mb-2">質問</p>
           <QuestionCard />
-          <Button className="mt-12   bg-[#FFFFFF] text-primary hover:text-[#FFFFFF] border border-primary">
-            <Plus className="mr-2 h-4 w-4" />
-            質問を追加
-          </Button>
+          <div className="flex justify-start">
+            <Button className="mt-6   bg-[#FFFFFF] text-primary hover:text-[#FFFFFF] border border-primary">
+              <Plus className="mr-2 h-4 w-4" />
+              質問を追加
+            </Button>
+          </div>
         </div>
       </div>
     </div>
