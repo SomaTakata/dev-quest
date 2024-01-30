2f3f2d933336bd379c76a2b3c1633468225e5fbe
Shogo Kitada <contact@shogo0x2e.com>
2024-01-30 15:58:21 +0900

    user は null ではありません！追加

--- app/(dev-quest)/(dashboard)/dashboard/page.tsx
+++ app/(dev-quest)/(dashboard)/dashboard/page.tsx
@@ -1,29 +1,19 @@
 import { currentUser } from "@clerk/nextjs";
 import DashBoard from "../_components/DashBoard";
 import { prisma } from "@/lib/prisma";
-import Link from "next/link";
 
 export default async function Home() {
   const user = await currentUser();
 
   // サインアップ後初回ログインの時は、user.id を DB に保存する
-  if (!user) {
-    return (
-      <div>
-        <p>以下のリンクから、サインインしてください。</p>
-        <Link href="/sign-in">サインインする</Link>
-      </div>
-    );
-  }
-
   const userInDB = await prisma.user.findUnique({
-    where: { id: user.id },
+    where: { id: user!.id },
   });
 
   if (!userInDB) {
     await prisma.user.create({
       data: {
-        id: user.id,
+        id: user!.id,
       },
     });
   }
