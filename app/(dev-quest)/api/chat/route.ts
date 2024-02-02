eec61d0074c0343977777999133f161822e833b8
Soma Takata <hayatetaka123@gmail.com>
2024-02-02 16:49:31 +0900

    APIの修正

---
+++ app/(dev-quest)/api/chat/route.ts
@@ -0,0 +1,53 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export async function POST(req: Request) {
+  try {
+    console.log("get start");
+
+    const { userId } = auth();
+    const body = await req.json();
+    const { question } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!configuration.apiKey) {
+      return new NextResponse("OpenAI API Key not configured.", {
+        status: 500,
+      });
+    }
+
+    if (!question) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `エントリーシートの質問を3つ以上のサブクエスチョンに分割ししてください。'。
+          出力は、json形式で、サブクエスチョンのみを返して`,
+        },
+        {
+          role: "user",
+          content: `質問：${question}`,
+        },
+      ],
+    });
+
+    return NextResponse.json(response.data.choices[0].message);
+  } catch (error) {
+    console.log("[CONVERSATION_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
