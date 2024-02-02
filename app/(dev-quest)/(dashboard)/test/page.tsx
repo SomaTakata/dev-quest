7919de22f7e402071695aebeff62d27290746036
Shogo Kitada <contact@shogo0x2e.com>
2024-02-02 14:59:33 +0900

    tRPC の初期設定と実装例を追加 (server/index.ts, (dashboard)/test/page.tsx)

---
+++ app/(dev-quest)/(dashboard)/test/page.tsx
@@ -0,0 +1,11 @@
+import Greeting1 from "./_components/Greeting1";
+
+const Home = () => {
+  return (
+    <div className="">
+      <Greeting1 />
+    </div>
+  );
+};
+
+export default Home;
---
+++ app/(dev-quest)/_trpc/provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import React, { useState } from "react";
+
+import { clientApi } from "./client-api";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient({}));
+  const [trpcClient] = useState(() =>
+    clientApi.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:3000/api/trpc",
+        }),
+      ],
+    }),
+  );
+  return (
+    <clientApi.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </clientApi.Provider>
+  );
+}
